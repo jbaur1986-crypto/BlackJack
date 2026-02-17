d9297c6c26714b04b0cad88e658ffce9a24290af
Johannes Baur <j.baur1986@gmail.com>
2026-02-18 00:16:43 +0100

    Pre-Shuffle-Mod

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,65 +1,157 @@
-﻿namespace BlackJack;
+﻿using System.ComponentModel;
 
-public class Hand
+namespace BlackJack
 {
-    private int _points;
 
-    public int Points => _points;
-
-    public void AddValue(int card)
+    public enum Color
     {
-        _points += card;
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
     }
-}
-public static class PlayerDecision
-{
-    public static bool WantsCard(Hand hand)
+
+    public enum Value
     {
-        if (hand.Points >= 22) return false;
-        
-        Console.WriteLine($"Punktestand: {hand.Points}");
-        Console.WriteLine("Möchten Sie noch eine Karte ziehen? (j/n)");
-        char input = Char.ToLower(Console.ReadKey().KeyChar);
-        while (input != 'j' && input != 'n')
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Ace,
+        Jack,
+        Queen,
+        King,
+
+    }
+
+    public static class CardSource
+    {
+        private static Random zufall = new Random();
+        private static List<Card> deck = new List<Card>();
+
+        private static void InitDeck()
         {
-            Console.WriteLine("Bitte richtig eingeben - 'j' für Karte 'n' für keine Karte ziehen! (j/n)");
-            input = Char.ToLower(Console.ReadKey().KeyChar);
+            deck.Clear();
+            
+            foreach (Color c in Enum.GetValues(typeof(Color)))
+            foreach (Value v in Enum.GetValues(typeof(Value)))
+                deck.Add(new Card(c, v));
+
+            Shuffle();
         }
-        return input == 'j';
-    }
-}
 
-public static class ComputerDecision
-{
-    public static bool WantsCard(Hand hand)
+        private static void Shuffle()
+        {
+            //Fisher-Yates-Shuffle
+        }
+    }
+
+    public class Card
     {
-        return hand.Points < 16;
-    }
-}
+        public Color Color { get; }
+        public Value Value { get; }
 
-public static class Rules
-{
-    public static bool IsFirstTurn(Hand hand)
+        public Card (Color color, Value value)
+        {
+            Color = color;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Color}";
+        }
+    }
+
+    public class Hand
     {
-        return true; //Logik fehlt noch!
+        public int Points { get; private set; }
+
+        public Hand()
+        {
+            Points = 0;
+        }
+
+        public void AddValue(int card)
+        {
+            if (card < 0)
+                throw new ArgumentException("card value cannot be negative.");
+            Points += card;
+        }
     }
-    //Doppelzug und Einfachzug, Check auf Bust, ggf. Einsätze???
-}
 
-public class PlayerRound
-{
-    
-}
 
-public class WinLossRound
-{
-    
-}
-
-class Program
-{
-    static void Main(string[] args)
+    public class Player
     {
-        Console.WriteLine("Hello, World!");
+        //player beitzt Hand, Lesbarkeit
+    }
+
+
+    public static class Decisions
+    {
+        public static bool PlayerWantsCard(Hand hand)
+        {
+            if (hand.Points >= 22) return false;
+
+            Console.WriteLine($"Punktestand: {hand.Points}");
+            Console.WriteLine("Möchten Sie noch eine Karte ziehen? (j/n)");
+            char input = Char.ToLower(Console.ReadKey().KeyChar);
+            while (input != 'j' && input != 'n')
+            {
+                Console.WriteLine("Bitte richtig eingeben - 'j' für Karte 'n' für keine Karte ziehen! (j/n)");
+                input = Char.ToLower(Console.ReadKey().KeyChar);
+            }
+
+            return input == 'j';
+        }
+
+        public static bool ComputerWantsCard(Hand hand)
+        {
+            return hand.Points < 16;
+        }
+
+        public static bool WinLossDecision()
+        {
+            return true; //fehlt
+        }
+
+
+    }
+
+
+    public static class Rules
+    {
+        public static bool CanDraw(Hand hand)
+        {
+            return true; //Logik fehlt noch!
+        }
+
+        public static bool IsBust(Hand hand)
+        {
+            return true; //fehlt noch
+        }
+    }
+
+
+
+
+    public static class Game
+    {
+        //PlayPlayerRound, ComputerRound, EvaluateWinLoss
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
+

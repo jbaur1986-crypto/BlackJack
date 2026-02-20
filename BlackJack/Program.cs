e739fc0d2f2180616fb37e2bd422b09258c6342d
Johannes Baur <j.baur1986@gmail.com>
2026-02-20 22:05:15 +0100

    CardAmountTest

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -2,7 +2,7 @@
 
 namespace BlackJack
 {
-                     
+
     public enum Suit
     {
         Hearts,
@@ -10,7 +10,7 @@ namespace BlackJack
         Spades,
         Clubs
     }
-                     
+
     public enum Value
     {
         Two = 2,
@@ -26,30 +26,35 @@ namespace BlackJack
         Jack,
         Queen,
         King,
-                     
+
     }
 
-                        
+
     public class ShoeOfDecks
     {
         private int counter;
         private List<Card> shoe = new List<Card>();
-        private readonly Random zufall = new Random(Guid.NewGuid().GetHashCode());// sicherer Seed
-                             
+        private readonly Random zufall = new Random(Guid.NewGuid().GetHashCode()); // sicherer Seed
+
         public ShoeOfDecks(int quantity)
         {
             InitShoeOfDecks(quantity);
         }
-                     
+
+        public IReadOnlyList<Card> Cards //Testproperty
+        {
+            get { return shoe; }
+        }
+
         private void InitShoeOfDecks(int quantity)
         {
             if (quantity <= 0) throw new ArgumentOutOfRangeException("Quantity must be at least 1.");
-            
+
             counter = 0;
             int i = 1;
-            
-            shoe.Clear();      
-            
+
+            shoe.Clear();
+
             while (i <= quantity)
             {
                 foreach (Suit c in Enum.GetValues<Suit>())
@@ -65,11 +70,11 @@ namespace BlackJack
 
             Shuffle();
         }
-                     
+
         private void Shuffle()
         {
             //Fisher-Yates-Shuffle rauf
-            for (int i = 0; i <= shoe.Count-2; i++)
+            for (int i = 0; i <= shoe.Count - 2; i++)
             {
                 int flag = zufall.Next(i, shoe.Count);
                 Card marked = shoe[i];
@@ -77,122 +82,140 @@ namespace BlackJack
                 shoe[flag] = marked;
             }
         }
-                             
+
 
         public Card Draw()
         {
             if (counter >= shoe.Count) throw new InvalidOperationException("No cards available.");
             counter++;
-            return shoe[counter-1];
-           
+            return shoe[counter - 1];
+
         }
-                             
+
     }
-                     
+
     public class Card
     {
         public Suit Suit { get; }
         public Value Value { get; }
-                     
-        public Card (Suit suit, Value value)
+
+        public Card(Suit suit, Value value)
         {
             Suit = suit;
             Value = value;
         }
-                     
+
         public override string ToString()
         {
             return $"{Value} of {Suit}";
         }
     }
-     ///////////////////////////////////////////////////////// Oben Refactored, unten alt.
-     /* 
-    public class Hand
-    {
-        public int Points { get; private set; }
+    ///////////////////////////////////////////////////////// Oben Refactored, unten alt.
+    /*
+   public class Hand
+   {
+       public int Points { get; private set; }
 
-        public Hand()
-        {
-            Points = 0;
-        }
+       public Hand()
+       {
+           Points = 0;
+       }
 
-        public void AddValue(int card)
-        {
-            if (card < 0)
-                throw new ArgumentException("card value cannot be negative.");
-            Points += card;
-        }
-    }
-
-
-    public class Player
-    {
-        //player beitzt Hand, Lesbarkeit
-    }
+       public void AddValue(int card)
+       {
+           if (card < 0)
+               throw new ArgumentException("card value cannot be negative.");
+           Points += card;
+       }
+   }
 
 
-    public static class Decisions
-    {
-        public static bool PlayerWantsCard(Hand hand)
-        {
-            if (hand.Points >= 22) return false;
-
-            Console.WriteLine($"Punktestand: {hand.Points}");
-            Console.WriteLine("Möchten Sie noch eine Karte ziehen? (j/n)");
-            char input = Char.ToLower(Console.ReadKey().KeyChar);
-            while (input != 'j' && input != 'n')
-            {
-                Console.WriteLine("Bitte richtig eingeben - 'j' für Karte 'n' für keine Karte ziehen! (j/n)");
-                input = Char.ToLower(Console.ReadKey().KeyChar);
-            }
-
-            return input == 'j';
-        }
-
-        public static bool ComputerWantsCard(Hand hand)
-        {
-            return hand.Points < 16;
-        }
-
-        public static bool WinLossDecision()
-        {
-            return true; //fehlt
-        }
+   public class Player
+   {
+       //player beitzt Hand, Lesbarkeit
+   }
 
 
-    }
+   public static class Decisions
+   {
+       public static bool PlayerWantsCard(Hand hand)
+       {
+           if (hand.Points >= 22) return false;
+
+           Console.WriteLine($"Punktestand: {hand.Points}");
+           Console.WriteLine("Möchten Sie noch eine Karte ziehen? (j/n)");
+           char input = Char.ToLower(Console.ReadKey().KeyChar);
+           while (input != 'j' && input != 'n')
+           {
+               Console.WriteLine("Bitte richtig eingeben - 'j' für Karte 'n' für keine Karte ziehen! (j/n)");
+               input = Char.ToLower(Console.ReadKey().KeyChar);
+           }
+
+           return input == 'j';
+       }
+
+       public static bool ComputerWantsCard(Hand hand)
+       {
+           return hand.Points < 16;
+       }
+
+       public static bool WinLossDecision()
+       {
+           return true; //fehlt
+       }
 
 
-    public static class Rules
-    {
-        public static bool CanDraw(Hand hand)
-        {
-            return true; //Logik fehlt noch!
-        }
-
-        public static bool IsBust(Hand hand)
-        {
-            return true; //fehlt noch
-        }
-    }
+   }
 
 
+   public static class Rules
+   {
+       public static bool CanDraw(Hand hand)
+       {
+           return true; //Logik fehlt noch!
+       }
+
+       public static bool IsBust(Hand hand)
+       {
+           return true; //fehlt noch
+       }
+   }
 
 
-    public static class Game
-    {
-        //PlayPlayerRound, ComputerRound, EvaluateWinLoss
-    }
-    */
-     
+
+
+   public static class Game
+   {
+       //PlayPlayerRound, ComputerRound, EvaluateWinLoss
+   }
+   */
+
     class Program
     {
         static void Main(string[] args)
         {
-            ShoeOfDecks shoe1 = new ShoeOfDecks(1);
-            foreach (var _ in Enumerable.Range(0, 52))
+            /*ShoeOfDecks shoe1 = new ShoeOfDecks(1);
+            foreach (var _ in Enumerable.Range(0, 53))
             {
                 shoe1.Draw();
+            }*/ //Test auf 52 Karten
+
+            ShoeOfDecks shoe2 = new ShoeOfDecks(5);
+            foreach (Card c in shoe2.Cards)
+            {
+                int cardCounter = 0;
+                foreach (Card d in shoe2.Cards)
+                {
+                    if (c.Suit == d.Suit && c.Value == d.Value)
+                    {
+                        cardCounter++;
+                    }
+                }
+
+                if (cardCounter != 5)
+                {
+                    throw new InvalidOperationException("Card number does not match.");
+                }
             }
         }
     }
